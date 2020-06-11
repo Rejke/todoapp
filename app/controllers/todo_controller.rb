62a6ff899c2bf0182ee37e6559182910913bbb9f
Rejke <Rejke@yandex.ru>
2020-06-11 17:20:46 +0300

    Done with todo#create action

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -7,5 +7,16 @@ class TodoController < ApplicationController
   end
 
   def create
+    @todo = Todo.new(post_params)
+    Project.all.each do |project|
+      if project.id == @todo.project_id then
+        project.todos << @todo
+      end
+    end
+    redirect_back(fallback_location: root_path)
+  end
+
+  private def post_params
+    params.require(:todo).permit(:text, :project_id)
   end
 end
