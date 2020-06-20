c5445d8c92102cbd7507765d803864a08ad5580b
Rejke <Rejke@yandex.ru>
2020-06-20 21:01:37 +0300

    Updates matches routes for API::Projects#index.json (api/projects/)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
+    match 'projects/' => 'projects#index', :via => [:get], :defaults => { :format => 'json' }
     get 'projects/index'
   end
 
